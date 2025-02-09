b23c33fb4a416a0587067a712d39a2421cd856af
sofiakoq <soffia.komarova@gmail.com>
2025-02-10 01:30:26 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,69 +1,55 @@
 import pygame
 import sys
-from pygame.locals import *
+from constants import screen, WHITE, BLACK, WIDTH, font, HEIGHT, RED
+from utils import distance
 
-from dots_lines import (
-    screen_w,
-    screen_h,
-    FPS,
-    BACKGROUND_COLOR,
-    Point,
-    Line,
-    generate_points
-)
+pygame.init()
 
 
-class Game:
-    def __init__(self, num_points):
-        self.screen = pygame.display.set_mode((screen_w, screen_h))
-        self.clock = pygame.time.Clock()
-        self.points = generate_points(num_points)
-        self.lines = []
-        self.selected_point = None
-        self.hovered_point = None
-
-    def run(self):
-        while True:
-            self.handle_events()
-            self.update()
-            self.draw()
-
-    def handle_events(self):
+def start_screen():
+    screen.fill(WHITE)
+    text = font.render("Головоломка 'Соедини точки'", True, BLACK)
+    screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))
+    text = font.render("Нажмите любую клавишу для начала", True, BLACK)
+    screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 + 50))
+    pygame.display.flip()
+    waiting = True
+    while waiting:
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                waiting = False
 
-            if event.type == MOUSEMOTION:
-                self.hovered_point = None
-                for point in self.points:
-                    if point.is_hovered(event.pos):
-                        self.hovered_point = point
-                        break
+def lose_screen():
+    screen.fill(WHITE)
+    text = font.render("Вы проиграли!", True, RED)
+    screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))
+    text = font.render("Нажмите любую клавишу, чтобы начать заново", True, BLACK)
+    screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 + 50))
+    pygame.display.flip()
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                waiting = False
 
-            if event.type == MOUSEBUTTONDOWN:
-                if self.hovered_point:
-                    if self.selected_point:
-                        if self.selected_point != self.hovered_point:
-                            self.selected_point.connected_to = self.hovered_point
-                            self.hovered_point.connected_to = self.selected_point
-                            self.lines.append(Line(self.selected_point, self.hovered_point))
-                            self.selected_point = None
-                    else:
-                        self.selected_point = self.hovered_point
-
-    def update(self):
-        pass
-
-    def draw(self):
-        self.screen.fill(BACKGROUND_COLOR)
-
-        for line in self.lines:
-            line.draw(self.screen)
-
-        for point in self.points:
-            point.draw(self.screen)
-
-        pygame.display.flip()
-        self.clock.tick(FPS)
-    
+def final_screen(level_count):
+    screen.fill(WHITE)
+    text = font.render(f"Поздравляем! Вы прошли {level_count} уровней!", True, BLACK)
+    screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))
+    text = font.render("Нажмите любую клавишу для выхода", True, BLACK)
+    screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 + 50))
+    pygame.display.flip()
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                waiting = False
