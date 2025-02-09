fa88f9b290ea94e0be2546dd25bc3504b409ac93
sofiakoq <soffia.komarova@gmail.com>
2025-02-10 01:31:42 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+font = pygame.font.Font(None, 36)
+WIDTH = 800
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
